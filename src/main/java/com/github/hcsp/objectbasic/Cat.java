0b7b4b024f4de916843109808fdbc5b956c5454d
catsme <845058547@qq.com>
2019-06-27 13:24:43 +0800

    实现了用方法重载的方式，为方法赋默认值。

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -11,13 +11,13 @@ public class Cat {
     /** 创建一只指定名字的猫，age的默认值是1，cute的默认值是true */
     public Cat(String name) {
         // 请在这里使用this()调用其他的构造器
-        this(name,1);
+        this(name,1);//给age设置默认值1
     }
 
     /** 创建一只指定名字和年龄的猫，cute的默认值是true */
     public Cat(String name, int age) {
         // 请在这里使用this()调用其他的构造器
-        this(name,age,true);
+        this(name,age,true);//由于age需要接收传进来的数据，所以只有cute可以设置默认值
     }
 
     /** 创建一只指定名字、年龄和萌属性的猫 */
