bb038e741f02658e92aceef09b94d65a0a1bb9c4
Zhang Xin <zhangxindev@gmail.com>
2019-07-16 00:19:24 +0800

    实现重载的构造器

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -18,7 +18,7 @@ public class Cat {
     /** 创建一只指定名字和年龄的猫，cute的默认值是true */
     public Cat(String name, int age) {
         // 请在这里使用this()调用其他的构造器
-        this(name, age, false);
+        this(name, age, true);
     }
 
     /** 创建一只指定名字、年龄和萌属性的猫 */
