8456c64c04f094b03b0897ddddafaee03d43f086
weiranyi <168578203@qq.com>
2020-11-30 17:28:55 +0800

    构造的重载

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -8,14 +8,20 @@ public class Cat {
     /** 猫咪是否萌，true为萌，false为不萌 */
     boolean cute;
 
+    public Cat(){
+        this("富林");
+    }
+
     /** 创建一只指定名字的猫，age的默认值是1，cute的默认值是true */
     public Cat(String name) {
         // 请在这里使用this()调用其他的构造器
+        this(name,1);
     }
 
     /** 创建一只指定名字和年龄的猫，cute的默认值是true */
     public Cat(String name, int age) {
         // 请在这里使用this()调用其他的构造器
+        this(name,age,true);
     }
 
     /** 创建一只指定名字、年龄和萌属性的猫 */
